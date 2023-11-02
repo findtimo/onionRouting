4a835855ccb36409a89a7ae70d8e86b8dd8af646
tkyj <82047680+tkyj@users.noreply.github.com>
2023-11-02 14:41:26 +0800

    Adced env file

--- src/config.js
+++ src/config.js
@@ -1,22 +1,23 @@
-// const config = {
-//   apiKey: 'AIzaSyCydxBW9ZBs-l_EX5v2hAD4-e1TkVQIf2o',
-//   authDomain: 'firedatatest-b55b1.firebaseapp.com',
-//   databaseURL: 'https://firedatatest-b55b1.firebaseio.com',
-//   projectId: 'firedatatest-b55b1',
-//   storageBucket: 'firedatatest-b55b1.appspot.com',
-//   messagingSenderId: '515900348614',
-//   appId: '1:515900348614:web:f769f8ea581a0cbffe9fc7'
-// }
-
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+
+// const config = {
+//   apiKey: "AIzaSyDiegodir8ePoMA7wu46J1SZm-qch4XUKQ",
+//   authDomain: "onionrouting-edd81.firebaseapp.com",
+//   databaseURL: "https://onionrouting-edd81-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "onionrouting-edd81",
+//   storageBucket: "onionrouting-edd81.appspot.com",
+//   messagingSenderId: "99795928320",
+//   appId: "1:99795928320:web:cb72ab826977a6b105dfe5",
+//   measurementId: "G-LHF6TB5PJ",
+// };
 
 export default config
